e89a3d431760a491bf70f3349e28dfd1da3c98ab
Ella Seibert <seiberea@mail.uc.edu>
2025-09-07 09:54:06 -0400

    Assignment Completion.

---
+++ src/main/java/com/seiberea/firstspringbootv2/FirstSpringBootV2Application.java
@@ -0,0 +1,13 @@
+package com.seiberea.firstspringbootv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootV2Application.class, args);
+    }
+
+}
